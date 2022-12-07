74120d46f62c68b3a68010f0b89bce619a7e26a4
tsuna23222 <richardcasinilloph@gmail.com>
2022-12-07 15:23:13 +0800

    java

--- javafodler/contact.js
+++ javafodler/contact.js
@@ -2,12 +2,15 @@ function contactbutton () {
     var fullname = document.getElementById("name").value;
     var emailadd = document.getElementById("email").value;
     var number = document.getElementById("number").value;
-    var commentery = document.getElementById("comment").valuek;
-
+    var commentery = document.getElementById("comment").value;
+    var contact = document.getElementById("contactus").value;
     if (fullname =="" && emailadd =="" && number =="" && commentery =="")
     {
         alert("Write Your Complain! :<");
         return false;
+    }
+    else if (contact ==""){
+        alert("Write Your Comment or feedback!!");
     }
     
     else 
